9493cd4e936e0550b1971006c53d1e0cef0f45c3
Slavus54 <54140819+Slavus54@users.noreply.github.com>
2019-10-12 20:25:49 +0700

    Add files via upload

--- My_Portfolio_3.0/main.js
+++ My_Portfolio_3.0/main.js
@@ -42,35 +42,3 @@ $(document).ready(function() {
 
 
 
-function hexoClock () {
-	var clock = document.getElementById('clock');
-	var color = document.getElementById('color');
-
-    var time = new Date();
-    var h = time.getHours().toString();
-    var m = time.getMinutes().toString();
-    var s = time.getSeconds().toString();
-
-    if (h.length < 2) {
-    	h = '0'+h;
-    }
-    if (m.length < 2) {
-    	m = '0'+m;
-    }
-    if (s.length < 2) {
-    	s = '0'+s;
-    }
-
-    var clockString = h+':'+m+':'+s;
-    var colorString = '#'+h+m+s;
-	
-	clock.textContent(clockString);
-    color.textContent(colorString);
-
-    document.getElementById('clocker').style.background = colorString;
-    
-}
-
-hexoClock ();
-setInterval(hexoClock, 1000);
-
